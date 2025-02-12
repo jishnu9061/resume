52a49b92b1052b5bc9e5e58e4bfd44b3580af381
JISHNU TK <jishnutkglobaleyet@gmail.com>
2025-02-12 15:59:49 +0530

    resume app installation completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function Header() {
+  return (
+    <Box>
+      Hi
+    </Box>
+  )
+}
+
+export default Header
---
+++ src/components/LayouRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LayouRoutes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LayouRoutes
